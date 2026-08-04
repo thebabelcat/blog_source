9d0cc43c8007a21128433130077adadf0d57e4ac
Leonardo Giordani <giordani.leonardo@gmail.com>
2026-08-04 14:54:43 +0100

    Latest version

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -12,7 +12,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "thebabelcat.github.io"
+SITEURL = "https://thebabelcat.github.io"
 RELATIVE_URLS = False
 DEBUG = False
 WEBASSETS_DEBUG = False
